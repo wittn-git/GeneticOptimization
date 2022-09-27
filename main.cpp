1901310a9be8e95b747cde75ffc84d266a18ad5c
wittn-git <wittner.dominic@gmail.com>
2022-09-27 22:17:05 +0200

    Impl. input by file; created example file

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include "parser/parser.hpp"
+#include "util/file_util.hpp"
 
 int main(){
     Parser parser;
-    Program prog = parser.parse("objective: 3x; constraints: 2+y+4z=5x;5y<=3x-1;");
+    std::string data = read_file("input.txt");
+    Program prog = parser.parse(data);
     std::cout << prog.to_string();
     return 0;
 }
---
+++ util/file_util.hpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+std::string read_file(std::string file_name){
+    std::fstream file;
+    file.open("../"+file_name, std::ios::in); 
+    if(file.is_open()){
+        std::string data, temp;
+        while(getline(file, temp)){  
+            data += temp;
+        }
+        file.close();
+        return data;
+   }
+   throw std::invalid_argument("Error when trying to open file");
+}
